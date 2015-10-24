46dc683720bb16e765b085ed1cc58c742feba834
mukulkhandelwal <mukulsuperstarrock@gmail.com>
2015-10-24 15:31:07 +0530

    changes added

---
+++ app/src/main/java/com/example/mukul/openyoutube/Config.java
@@ -0,0 +1,14 @@
+package com.example.mukul.openyoutube;
+
+/**
+ * Created by mukul on 10/21/2015.
+ */
+
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyABYoczeHg4XABx_jMRfv-CqmA2YMsIY4A";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
